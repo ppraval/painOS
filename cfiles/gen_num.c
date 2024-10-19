0b7dd5eda82186bcc5a974739a192392ab188bb6
iamTHOPE <ppraval@live.com>
2024-10-20 02:24:26 +0530

    Stage 24 done. Ran into a weird problem

---
+++ cfiles/gen_num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+    FILE* fptr;
+    fptr = fopen("../datafiles/numbers.dat", "w");
+    int i = 1;
+    for (i = 1; i <= 2047; i++) 
+    {
+        fprintf(fptr, "%d\n", i);
+    }
+    fclose(fptr);
+    return 0;
+}
