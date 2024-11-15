0346b58f6797a6c465e4b3e8653438740a8f3deb
iamTHOPE <ppraval@live.com>
2024-11-16 00:33:55 +0530

    Forgot to add the final stage and also removed all the debugging messages

---
+++ expl/compileexpl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+./expl ../stage27/create.expl
+./expl ../stage27/delete.expl
+./expl ../stage27/idle.expl
+./expl ../stage27/init.expl
+./expl ../stage27/m_merge.expl
+./expl ../stage27/m_sort.expl
+./expl ../stage27/m_store.expl
+./expl ../stage27/merge.expl
+./expl ../stage27/pgm1.expl
+./expl ../stage27/pgm2.expl
+./expl ../stage27/shell.expl
+./expl ../stage27/test11.expl
+./expl ../stage27/test8.expl
+./expl ../stage27/test9.expl
